26761883d5ccd651d5d86ed7ff11966d19604dc8
Hikaru Sato <hisato@solxyz.co.jp>
2018-12-06 17:09:40 +0900

    設定変更

--- mysql/docker-entrypoint-initdb.d/sakai-db-setup.sh
+++ mysql/docker-entrypoint-initdb.d/sakai-db-setup.sh
@@ -1,3 +1,4 @@
 mysql -uroot -ppassword -h localhost -e "create database sakai default character set utf8;"
-mysql -uroot -ppassword -h localhost -e "grant all privileges on sakai.* to 'sakai'@'%' identified by 'ironchef';"
-mysql -uroot -ppassword -h localhost -e "grant all privileges on sakai.* to 'sakai'@'%' identified by 'ironchef';"
+mysql -uroot -ppassword -h localhost -e "create user 'sakai'@'%' identified by 'ironchef';"
+mysql -uroot -ppassword -h localhost -e "grant all privileges on sakai.* to 'sakai'@'%';"
+mysql -uroot -ppassword -h localhost -e "FLUSH PRIVILEGES;"
